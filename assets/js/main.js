a703053c0915675b3d1603474456c5be279df4e3
Никита-Егунов <negunov1003@gmail.com>
2024-08-05 09:45:02 +0300

    Почти готово

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var mainButtons = document.querySelectorAll('.mainButton');
+
+    mainButtons.forEach(function(mainButton) {
+        mainButton.addEventListener('click', function() {
+            var subButtons = mainButton.nextElementSibling;
+            if (subButtons.classList.contains('hidden')) {
+                subButtons.classList.remove('hidden');
+            } else {
+                subButtons.classList.add('hidden');
+            }
+        });
+    });
+});
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+    var swiper = new Swiper(".tariff1__Swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+    });
